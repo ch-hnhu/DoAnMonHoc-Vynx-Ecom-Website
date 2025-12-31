52bacb37e2ab12ef5a590dcb95dcc617187cec7d
Chung Hue Nhu <0306231416@caothang.edu.vn>
2025-12-31 02:02:58 -0800

    first commit

---
+++ backend/routes/api.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+// Public routes - Test
+Route::get('/', function () {
+    return response()->json([
+        'message' => 'API is working!',
+        'timestamp' => now(),
+    ]);
+});
+
+Route::get('/test', function () {
+    return response()->json([
+        'message' => 'hehe tako nek!'
+    ]);
+});
+
+// Protected routes - Require authentication
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+});
